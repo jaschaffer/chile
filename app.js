2574712324499054ff550b24b6030b189c1ed49c
jaschaffer <james_schaffer@cs.ucsb.edu>
2015-07-04 20:24:48 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+var exec = require('child_process').exec;
+var uuid = require('node-uuid');
+var path = require('path');
+var mime = require('mime');
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var app = express();
+var request = require('request');
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer({ dest: './uploads/'}));
+app.use('/facebook', express.static('public'));
+app.post("/facebook", function(req, res, next) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/image', function(req, res){
+  console.log(req.body);
+  var postData = req.body.url;
+  var tempName = uuid.v1();
+  download(postData, tempName + '.jpg', function(){
+    var file = tempName + '.jpg';
+    child = exec('composite -gravity center flag.jpg[390x^] -geometry +115-0 ' + file + '[390x] -dissolve 30 ' + file,
+      function (error, stdout, stderr) {
+        var filename = path.basename(file);
+        var mimetype = mime.lookup(file);
+        res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+        res.setHeader('Content-type', mimetype);
+
+        var filestream = fs.createReadStream(file);
+        filestream.pipe(res);
+    });
+  });
+});
+
+app.listen(process.env.PORT || 8000);
---
+++ public/lib/js/app.js
@@ -0,0 +1,76 @@
+var imgUrl = '';
+//$('.fb-welcome, #start-text, #loginbtn, #loadimg, #lower-div').toggle();
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: '851549238270521',
+    xfbml: true,
+    version: 'v2.3'
+  });
+  function onLogin(response) {
+    if (response.status == 'connected') {
+      FB.api(
+        "/me/picture?width=400&height=400",
+        function (response) {
+          if (response && !response.error) {
+            imgUrl = response.data.url;
+            document.getElementById('profile-pic').src = imgUrl;
+            document.getElementById('filename').value = imgUrl;
+            $('.fb-welcome, #start-text, #loginbtn, #loadimg, #lower-div').toggle();
+          }
+        }
+      );
+      FB.api('/me?fields=first_name', function(data) {
+        var welcomeBlock = document.getElementById('fb-welcome');
+        welcomeBlock.innerHTML = 'Hi, ' + data.first_name + '.';
+      });
+    }
+  }
+
+  FB.getLoginStatus(function(response) {
+    if (response.status == 'connected') {
+      onLogin(response);
+    } else {
+      // Otherwise, show Login dialog first.
+      FB.login(function(response) {
+        onLogin(response);
+      }, {scope: 'user_photos'});
+    }
+  });
+};
+
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function post() {
+  FB.api(
+    "/me/feed",
+    "POST",
+    {
+        "message": "I just celebrated Independence Day with https://apps.facebook.com/celebratejulyfourth.",
+        "link": "https://apps.facebook.com/celebratejulyfourth"
+    },
+    function (response) {
+      if (response && !response.error) {
+        $('#myform').submit();
+      }
+    }
+);
+}
+
+function login() {
+  FB.getLoginStatus(function(response) {
+    if (response.status == 'connected') {
+      onLogin(response);
+    } else {
+      // Otherwise, show Login dialog first.
+      FB.login(function(response) {
+        onLogin(response);
+      }, {scope: 'user_photos'});
+    }
+  });
+}
